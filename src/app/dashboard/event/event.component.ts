f95093bfc350ef8ee0280a8b5c50d4a08ee088c7
Jaider Mendoza <jaiderstyvenmendoza@gmail.com>
2024-04-16 17:05:50 -0500

    feat: :lipstick: styles for event component

--- src/app/dashboard/event/event.component.ts
+++ src/app/dashboard/event/event.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-event',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, MatDividerModule],
   templateUrl: './event.component.html',
   styleUrl: './event.component.css'
 })
