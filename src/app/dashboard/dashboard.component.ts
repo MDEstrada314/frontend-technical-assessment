93a5014dc8faf7c08380ee2b7c99a89ddf6afa57
Jaider Mendoza <jaiderstyvenmendoza@gmail.com>
2024-04-16 16:08:33 -0500

    feat: :lipstick: preview components

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "./header/header.component";
+import { BetslipComponent } from "./betslip/betslip.component";
+import { EventComponent } from "./event/event.component";
 
 @Component({
-  selector: 'app-dashboard',
-  standalone: true,
-  imports: [],
-  templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+    selector: 'app-dashboard',
+    standalone: true,
+    templateUrl: './dashboard.component.html',
+    styleUrl: './dashboard.component.css',
+    imports: [HeaderComponent, BetslipComponent, EventComponent]
 })
 export class DashboardComponent {
 
--- src/app/dashboard/event/event.component.ts
+++ src/app/dashboard/event/event.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-event',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './event.component.html',
   styleUrl: './event.component.css'
 })
